11c8d986c0e1ef3920913bd554b9acf84d77f724
Yoshi <mineyoshiyuki@TopHope.local>
2020-06-05 11:55:24 +0900

    feet(create):Create画面追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//追加
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule, // 追加
+    ReactiveFormsModule // 追加
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AppComponent],
+  bootstrap: [AppComponent],
+  declarations: [AppComponent]
 })
 export class AppModule { }
